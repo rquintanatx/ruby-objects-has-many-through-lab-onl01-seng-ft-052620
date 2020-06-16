87231e9eac82df2577ac36ec95e2eec2fbe6d4c3
Robert Quintana <robertquintanatx@gmail.com>
2020-06-16 18:39:14 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,32 @@
+require 'pry'
+class Doctor 
+  attr_accessor :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end 
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+  
+  def appointments 
+    Appointment.all.select do |appointment| 
+      appointment.doctor == self
+    end 
+  end 
+  
+  def patients
+    
+    appointments.collect do |appointment| 
+      appointment.patient
+    end 
+  end 
+  
+  def self.all 
+    @@all 
+  end
+  
+end 
