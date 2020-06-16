54f4a0934c2129c55da3b0e1b975b2e62d7b8c95
Robert Quintana <robertquintanatx@gmail.com>
2020-06-16 17:08:26 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,19 @@
+class Artist 
+  attr_accessor :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def songs 
+    Song.all.select do |song|
+      song.artist == self
+    end 
+  end 
+  
+  def self.all
+    @@all
+  end
+end 
